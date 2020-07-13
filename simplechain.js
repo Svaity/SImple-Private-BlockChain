29e952c32cc6757a8d6ef9d20364b3b6d5824c3f
Shrey <svaity@stevens.edu>
2020-07-13 16:54:12 -0400

    Linking Blocks

--- simplechain.js
+++ simplechain.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 //Block Class
 class Block {
   constructor(data) {
@@ -5,7 +7,7 @@ class Block {
       (this.height = 0),
       (this.body = data),
       (this.time = 0),
-      (this.previousblockhash = "");
+      (this.previousBlockHash = "");
   }
 }
 
@@ -13,8 +15,13 @@ class Block {
 class BlockChain {
   constructor() {
     this.chain = [];
+    this.addBlock(new Block("First Block in chain- Genesis Block"));
   }
   addBlock(newBlock) {
+    if (this.chain.length > 0) {
+      newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash;
+    }
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     this.chain.push(newBlock);
   }
 }
